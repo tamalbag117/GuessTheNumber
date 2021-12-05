84cbac9ee8c52916a22fcf1f8e032788d59b6ac6
TAMAL BAG <87414354+tamalbag117@users.noreply.github.com>
2021-12-05 17:36:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/guessthenumber/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.guessthenumber
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage : ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Get the Button view from the layout and assign a click
+        // listener to it.
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener { rollDice() }
+        diceImage = findViewById(R.id.dice_image)
+    }
+
+    /**
+     * Click listener for the Roll button.
+     */
+    private fun rollDice() {
+        // Toast.makeText(this, "button clicked",
+        //  Toast.LENGTH_SHORT).show()
+
+        val drawableResource = when ((1..6).random()) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+    }
+}
