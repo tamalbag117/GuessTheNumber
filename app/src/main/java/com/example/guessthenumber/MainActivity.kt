2d1d85ca41bf42a251e9e9683829f5d3630c383a
TAMAL BAG <87414354+tamalbag117@users.noreply.github.com>
2021-12-05 17:39:30 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/guessthenumber/MainActivity.kt
+++ app/src/main/java/com/example/guessthenumber/MainActivity.kt
@@ -14,21 +14,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // Get the Button view from the layout and assign a click
-        // listener to it.
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
         diceImage = findViewById(R.id.dice_image)
     }
-
-    /**
-     * Click listener for the Roll button.
-     */
     private fun rollDice() {
-        // Toast.makeText(this, "button clicked",
-        //  Toast.LENGTH_SHORT).show()
-
         val drawableResource = when ((1..6).random()) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
